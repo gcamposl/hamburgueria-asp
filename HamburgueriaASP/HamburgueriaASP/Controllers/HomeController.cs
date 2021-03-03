59df6b8228794bf14f78e9c8505087b425f75a92
Gabriel Campos Lopes <gcamposlopes@gmail.com>
2021-03-03 20:12:28 -0300

    project created

---
+++ HamburgueriaASP/HamburgueriaASP/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using HamburgueriaASP.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HamburgueriaASP.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    }
+}
